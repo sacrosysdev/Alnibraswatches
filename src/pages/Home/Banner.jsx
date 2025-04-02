3257fd12c2dfbf67e447cf3798e661ef79afb219
Shinas Muhammed <shinasmuhd43@gmail.com>
2025-04-02 17:12:29 +0530

    mobile banner

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -7,15 +7,17 @@ import { features } from '../../constants'
 const Banner = () => {
   return (
     <div className=''>
-      <div className='relative h-[110vh] md:h-[50vh] xl:min-h-[130vh] overflow-hidden'>
+      <div className='relative h-[120vh] md:h-[70vh] xl:min-h-[130vh] overflow-hidden'>
         <img src={BannerImage} alt="" className=' h-full w-full object-cover'/>
         <div className='absolute left-2 xl:left-16 top-5 xl:top-16 w-full'>
             <h1 className='md:w-1/2 font-unlock text-3xl xl:text-[60px] xl:leading-16 text-white'>Discover the Future of Watch E-Commerce: Unleash Your Style!</h1>
             <p className='md:w-1/3 pt-5 font-extralight leading-5 text-white'>Explore the next generation of watch shopping with our cutting-edge e-commerce platform, featuring extraordinary craftsmanship and one-of-a-kind styles.</p>
         </div>
         <div className='absolute bottom-0 py-14 backdrop-blur-md w-full flex xl:gap-10 justify-center items-center'>
-            <div className='flex absolute -top-40 md:bottom-16 xl:-bottom-10'>
-              <img src={Watch} alt="" className='object-cover '/>
+            <div className='flex absolute bottom-56  md:-bottom-3 lg:-bottom-10 xl:-bottom-10 '>
+              <div className='min-h-[70vh]'>
+                <img src={Watch} alt="" className='object-cover w-full h-full '/>
+              </div>
             </div>
             <div className='grid grid-cols-2 md:grid-cols-4 gap-3'>
               {features.map((item, index)=>(<div key={index} className='flex flex-col gap-2 items-center '>
