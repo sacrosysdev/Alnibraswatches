3ef1f990c23f0e332cf57054c7effc1e592b65f0
Shinas Muhammed <shinasmuhd43@gmail.com>
2025-04-09 18:48:14 +0530

    navbar height adjusting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -104,11 +104,11 @@ const Navbar = () => {
     initial={{ width: "100%" }}
     animate={{ width: cartOpen ? "75%" : "100%" }}
     transition={{ type: "tween", duration: 0.3, ease: "easeInOut" }}
-    className={`   bg-[#005C53] px-5 xl:px-16 py-5 xl:py-2 z-50 ${navSticky ? 'fixed top-0 left-0 right-0 shadow-md ' : 'relative'}`}>
+    className={`   bg-[#005C53] px-5 xl:px-16 py-2 z-50 ${navSticky ? 'fixed top-0 left-0 right-0 shadow-md ' : 'relative'}`}>
       <div className='flex justify-between items-center'>
         
-          <div className='w-1/3'>
-            <img src={Logo} alt="logo" className='object-cover xl:object-contain min-h-10 min-w-20' />
+          <div className='w-1/3 '>
+            <img src={Logo} alt="logo" className='object-cover xl:object-contain min-h-10 min-w-16 max-h-20' />
           </div>
         
         <div className='w-1/2'>
